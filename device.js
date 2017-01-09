4ba22fea6a8d78bcee85ce20846c8dc1ddc93bd5
piggysticker <641777437@qq.com>
2017-01-09 14:35:41 +0800

    device模块 调试OK

--- device.js
+++ device.js
@@ -5,6 +5,7 @@
 var device = exports;
 
 var http = require('http');
+var config = require('./config.json');
 
 device.post = function (req, res, next) {
     console.log('POST '+ req.url);
@@ -17,37 +18,24 @@ device.post = function (req, res, next) {
     else
     {
         var transdata = JSON.stringify(req.body);
-        console.log('transdata:', transdata);
-        var options = {
-            host: "test.xiaoan110.com",
-            port: 8082,
-            path: "/v1/device",
-            method: "POST",
-            headers: {
-                "Content-Type": 'application/json',
-                "Content-Length": transdata.length
-            }
-        };
-        var requset = http.request(options, function (response) {
-            console.log('status:', response.statusCode);
-            console.log('headers:', response.headers);
+        console.log('app2dev:', transdata);
+
+        var requset = http.request(config.httppost, function (response) {
             if (response.statusCode === 200) {
                 var body = "";
                 response.on('data', function (data) {
                     body += data;
-                    console.log(data);
-                }).on('end', function () {
-                    console.log('json:', JSON.parse(body));
-                    res.send(200, JSON.parse(body));
-                })
+                });
+                response.on('end', function() {
+                    res.send(JSON.parse(body));
+                    console.log('dev2app:', body);
+                });
             }
             else {
-                res.send({code:100});
+                res.send({code: 100});
             }
         });
-        requset.write(transdata);
-        requset.end();
+        requset.end(transdata);
     }
-
     return next();
 };
